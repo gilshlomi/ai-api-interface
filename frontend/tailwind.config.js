9ded9753974da540d223b594e5c4d695282ffb00
gilshlo <89919165+gilshlo@users.noreply.github.com>
2025-04-01 19:03:07 +0300

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./src/**/*.{js,ts,jsx,tsx,mdx}", // This will include all files in the src directory
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-      extend: {
-        // Your custom theme extensions
-      },
+      extend: {},
     },
     safelist: [
-      'bg-white', // Explicitly safelist bg-white
+      'bg-white',
+      'text-gray-900',
+      'border-gray-200',
+      'text-gray-700',
+      'hover:bg-gray-200',
+      'bg-gray-100'
     ],
     plugins: [],
   }
